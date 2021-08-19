7be582493c6303e879e4d6c04fb71e574654e982
hmuronaka <capriccio27@gmail.com>
2021-08-19 11:37:47 +0900

    Initial Commit

---
+++ MouseHookSample/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MouseHookSample
+//
+//  Created by MuronakaHiroaki on 2021/08/19.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
